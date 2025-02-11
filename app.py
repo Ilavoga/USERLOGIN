f43dbdb3fb6827fec7c902c195bf856219c96e8e
Sandra Ilavoga <skisambo@kabarak.ac.ke>
2025-02-11 12:58:32 +0300

    templates added

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Home"
+    return render_template('home.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
